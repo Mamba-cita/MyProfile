14efe664c7a21a60dad9f5ada66434a60c37bc10
Mamba-Cita <edwnsjohn52@gmail.cm>
2022-05-23 17:45:03 +0300

    searchbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './Main.scss';
 import SidebarNav from './Componets/Navbar/Navbarscripts/SidebarNav';
+import SearchBar from './Componets/Searchbar/SearchBar';
 
 function App() {
   return (
     <>
 <SidebarNav />
+
+<SearchBar/>
 
 
 
